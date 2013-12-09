616d7d01f5b135b318fc1046f534d24ac30cc32d
Guilherme Guerra <guerrinha@comum.org>
2013-12-09 17:58:47 -0200

    Adding slideshow

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+    orbit: {
+        animation: 'slide',
+        timer_speed: 1000,
+        pause_on_hover: true,
+        animation_speed: 500,
+        navigation_arrows: true,
+        bullets: false,
+        timer: false,
+        slide_number: false,
+        next_on_click: true
+    }
+});
